1006b527380e13cb8f61d3e0b8b2c1bf0eb7c748
Elena Rudenko <rdnk_e@mail.ru>
2023-10-20 19:17:44 +0300

    Initial commit

---
+++ src/test/java/com/example/randomcoffee/RandomCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.randomcoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
