986740a7dc1da2ccbeba78fbff2c9d9f5bad501f
Elena Rudenko <elru0121@netcracker.com>
2023-11-13 19:03:22 +0300

    Tests added

---
+++ src/test/java/com/example/randomcoffee/service/impl/UserServiceImplTest.java
@@ -0,0 +1,44 @@
+package com.example.randomcoffee.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void getUserDto() {
+    }
+
+    @Test
+    void usersByLastName() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void allUsers() {
+    }
+
+    @Test
+    void checkAllUserEvents() {
+    }
+
+    @Test
+    void userChangeOffice() {
+    }
+
+    @Test
+    void findByHiringPeriod() {
+    }
+}
